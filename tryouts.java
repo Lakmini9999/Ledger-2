4bb19520ac882c378e923adbcba7f47d232d73bb
Lakmini9999 <manthrirathna.mal@gmail.com>
2019-01-07 09:45:02 +0530

    first

---
+++ tryouts.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my.ims;
+
+import javax.swing.JApplet;
+
+/**
+ *
+ * @author Lucky
+ */
+public class tryouts extends JApplet {
+
+    /**
+     * Initialization method that will be called after the applet is loaded into
+     * the browser.
+     */
+    public void init() {
+        // TODO start asynchronous download of heavy resources
+    }
+    // TODO overwrite start(), stop() and destroy() methods
+}
